931cd97772e5da481d22f48ce574843638cf4783
Kritika Singh <kritikasingh00.com@gmail.com>
2023-12-02 10:14:03 +0530

    starting my project

---
+++ src/_auth/forms/AuthLayout.tsx
@@ -0,0 +1,22 @@
+import { Outlet, Navigate } from "react-router-dom";
+
+const AuthLayout = () => {
+  const isAuthenticated = false;
+  return (
+    <div>
+      <> 
+      {isAuthenticated? (
+        <Navigate to="/"/>
+      ) :(
+        <>
+        <section>
+          <Outlet />
+        </section>
+        </>
+      )}
+      </>
+    </div>
+  )
+}
+
+export default AuthLayout
---
+++ src/_root/RootLayout.tsx
@@ -0,0 +1,9 @@
+const RootLayout = () => {
+  return (
+    <div>
+      hi
+    </div>
+  )
+}
+
+export default RootLayout
